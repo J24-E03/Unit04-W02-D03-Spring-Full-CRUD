404d2a686d175ff8da6edbcc2bf593771a5bee8b
maryam <maryam.ghayoomi@gmail.com>
2025-05-07 15:09:17 +0200

    implements BONUS: Client-Side Validation

---
+++ labs/booklist/src/main/resources/static/js/book-form.js
@@ -0,0 +1,11 @@
+console.log("my javascript file is loaded")
+const inputElement = document.querySelectorAll("input")
+
+inputElement.forEach((oneInput)=>{
+    oneInput.addEventListener("invalid",(e)=>{
+        e.preventDefault()
+
+        document.querySelector(`#${oneInput.id}Error`)
+            .style.display="block"
+    })
+})
